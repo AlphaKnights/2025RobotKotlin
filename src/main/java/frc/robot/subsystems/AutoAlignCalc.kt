526dc476cd608585824a75a0230d2bbda363209f
Jsohn-1 <proepicgamer1@gmail.com>
2025-04-19 17:33:36 -0700

    Fix autoalign and tests

--- src/main/java/frc/robot/subsystems/AutoAlignCalc.kt
+++ src/main/java/frc/robot/subsystems/AutoAlignCalc.kt
@@ -12,34 +12,38 @@ object AutoAlignCalc {
         curPose: Pose3d
     ) : ChassisSpeeds {
         // Units are in meters and radians
-        var x: Double = curPose.x
+        var x: Double = -curPose.x // Inverted to match the robot's coordinate system
         var z: Double = curPose.z
         val yaw: Double = curPose.rotation.z
 
+        var x_normalized = 1.0
+        var z_normalized = 1.0
+
         // Calculate the offsets for the Apriltag
-        x -= (cos(yaw) * goalZ) - (sin(yaw) * goalX)
-        z += (sin(yaw) * goalZ) + (cos(yaw) * goalX)
+        x -= (sin(yaw) * goalZ) + (cos(yaw) * goalX)
+        z += -(cos(yaw) * goalZ) - (sin(yaw) * goalX)
+
+        // Calculate the distance to the target
+        var dist = sqrt(x.pow(2) + z.pow(2))
+        dist = if (dist > Constants.AlignConstants.FINE_ALIGN_DEADZONE) 1.0 else dist / Constants.AlignConstants.FINE_ALIGN_DEADZONE
 
         // Normalize the x and z values
         if (abs(x) > abs(z)) {
-            z = abs(z) / abs(x)
-            x = 1.0
+            z_normalized = abs(z) / abs(x)
+            x_normalized = 1.0
         }
         if (abs(x) < abs(z)) {
-            z = 1.0
-            x = abs(x) / abs(z)
+            z_normalized = 1.0
+            x_normalized = abs(x) / abs(z)
         }
 
-        if (z < 0) z *= -1.0
+        if (z < 0) z_normalized *= -1.0
 
-        if (x < 0) x *= -1.0
+        if (x > 0) x_normalized *= -1.0
 
-        x = if (x < Constants.AlignConstants.ALIGN_DEADZONE) 0.0 else x
-        z = if (z < Constants.AlignConstants.ALIGN_DEADZONE) 0.0 else z
+        x_normalized = if (abs(x) < Constants.AlignConstants.ALIGN_DEADZONE) 0.0 else x_normalized
+        z_normalized = if (abs(z) < Constants.AlignConstants.ALIGN_DEADZONE) 0.0 else z_normalized
 
-        var dist = sqrt(x.pow(2) + z.pow(2))
-
-        dist = if (dist > Constants.AlignConstants.FINE_ALIGN_DEADZONE) 1.0 else dist / Constants.AlignConstants.FINE_ALIGN_DEADZONE
 
         // Calculate the rotation speed
         val rotSign: Double = if (abs(yaw) < Constants.AlignConstants.ALIGN_ROT_DEADZONE) 0.0 else yaw/abs(yaw)
@@ -51,9 +55,9 @@ object AutoAlignCalc {
         angularDistance = sqrt(angularDistance)
 
         return ChassisSpeeds(
-            z * dist * Constants.AlignConstants.MAX_SPEED,
-            -x * dist * Constants.AlignConstants.MAX_SPEED,
-            rotSign * angularDistance * Constants.AlignConstants.MAX_ANGULAR_SPEED,
+            z_normalized * dist * Constants.AlignConstants.MAX_SPEED,
+            -x_normalized * dist * Constants.AlignConstants.MAX_SPEED,
+            -rotSign * angularDistance * Constants.AlignConstants.MAX_ANGULAR_SPEED,
         )
 
     }
--- src/test/java/frc/robot/AutoAlignTest.kt
+++ src/test/java/frc/robot/AutoAlignTest.kt
@@ -17,34 +17,20 @@ internal class AutoAlignTest {
             arrayOf(
                 Pose3d(
                     Translation3d(
-                        0.0, 0.0, 1.0
+                        0.04, 0.0, 0.5
                     ),
                     Rotation3d(
-                        0.0, 0.0, 0.0
+                        0.0, 0.0, Math.PI / 6
                     ),
                 ),
                 doubleArrayOf(
-                    0.0, 0.0
+                    0.4, 0.3
                 ),
                 ChassisSpeeds(
-                    1.0, -0.0, 0.0
+                    0.07492844421746891, -1.0, -1.0
                 ),
-
-                Pose3d(
-                    Translation3d(
-                        0.4, 0.0, 0.3
-                    ),
-                    Rotation3d(
-                        0.0, 0.0, PI / 6
-                    ),
-                ),
-                doubleArrayOf(
-                    0.3, 0.4
-                ),
-                ChassisSpeeds(
-                    -0.6469387689401926, 1.0, 0.0
-                ),
-
+            ),
+            arrayOf(
                 Pose3d(
                     Translation3d(
                         0.5, 0.0, 2.0
@@ -57,11 +43,27 @@ internal class AutoAlignTest {
                     -0.3, 0.2
                 ),
                 ChassisSpeeds(
-                    1.0, -0.34884414872803654, 0.0
+                    1.0, -0.08360734950696878, 1.0
                 ),
-
+            ),
+            arrayOf(
+                Pose3d(
+                    Translation3d(
+                        -1.2, 0.0, 2.3
+                    ),
+                    Rotation3d(
+                        0.0, 0.0, -PI / 12
+                    ),
+                ),
+                doubleArrayOf(
+                    0.4, 0.2
+                ),
+                ChassisSpeeds(
+                    1.0, 0.3915200911131215, 0.8660254037844386
+                ),
             )
         )
+
         for (test in mockPoses) {
             val pose = test[0] as Pose3d
             val offsets = test[1] as DoubleArray
@@ -70,6 +72,7 @@ internal class AutoAlignTest {
             val result = AutoAlignCalc.getAlignSpeeds(offsets[0], offsets[1], pose)
             assertEquals(expectedChassisSpeeds.vxMetersPerSecond, result.vxMetersPerSecond)
             assertEquals(expectedChassisSpeeds.vyMetersPerSecond, result.vyMetersPerSecond)
+            assertEquals(expectedChassisSpeeds.omegaRadiansPerSecond, result.omegaRadiansPerSecond)
         }
     }
 
@@ -83,12 +86,31 @@ internal class AutoAlignTest {
                 0.0, 0.0, 0.0
             ),
         )
-        val expectedChassisSpeeds = ChassisSpeeds(0.0, -0.0, 0.0)
+        val expectedChassisSpeeds = ChassisSpeeds(0.0, -0.0, -0.0)
 
         val result = AutoAlignCalc.getAlignSpeeds(0.0, 0.0, pose)
         assertEquals(expectedChassisSpeeds.vxMetersPerSecond, result.vxMetersPerSecond)
         assertEquals(expectedChassisSpeeds.vyMetersPerSecond, result.vyMetersPerSecond)
+        assertEquals(expectedChassisSpeeds.omegaRadiansPerSecond, result.omegaRadiansPerSecond)
     }
+
+@Test
+fun `getAlignSpeeds returns left when tag is to the left`() {
+    val pose = Pose3d(
+        Translation3d(
+            0.5, 0.0, 0.0
+        ),
+        Rotation3d(
+            0.0, 0.0, 0.0
+        ),
+    )
+    val expectedChassisSpeeds = ChassisSpeeds(0.0, -1.0, -0.0)
+
+    val result = AutoAlignCalc.getAlignSpeeds(0.0, 0.0, pose)
+    assertEquals(expectedChassisSpeeds.vxMetersPerSecond, result.vxMetersPerSecond)
+    assertEquals(expectedChassisSpeeds.vyMetersPerSecond, result.vyMetersPerSecond)
+    assertEquals(expectedChassisSpeeds.omegaRadiansPerSecond, result.omegaRadiansPerSecond)
+}
 
     @Test
     fun `getAlignSpeeds returns correct speeds when directly behind tag`() {
@@ -101,7 +123,7 @@ internal class AutoAlignTest {
             ),
         )
         val offsets = doubleArrayOf(0.0, 0.0)
-        val expectedChassisSpeeds = ChassisSpeeds(1.0, -0.0, 0.0)
+        val expectedChassisSpeeds = ChassisSpeeds(1.0, -0.0, -0.0)
 
         val result = AutoAlignCalc.getAlignSpeeds(offsets[0], offsets[1], pose)
         assertEquals(expectedChassisSpeeds.vxMetersPerSecond, result.vxMetersPerSecond)
